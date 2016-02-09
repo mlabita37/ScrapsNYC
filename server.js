712ee49dad180d53aefff42cabe8da5883250e67
Mike Labita <mlabita37@gmail.com>
2016-02-09 14:22:09 -0500

    Creates ScrapsNYC app

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app     = express();
+
+app.use( express.static('./public') );
+
+app.get('/', function(req, res, next){
+  res.sendFile(__dirname + '/public/views/index.html');
+});
+
+var port = 8080;
+app.listen(port, function(){
+  console.log("...listending on " + port);
+});
