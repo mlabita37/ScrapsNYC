144127cf77b939199ebef1e9b5791a6465519d1d
Mike Labita <mlabita37@gmail.com>
2016-04-14 08:55:47 -0400

    Adds New Relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Scraps NYC'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '9c066cc19c1ba527a140598f9f3a2b02d2d847ac',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
